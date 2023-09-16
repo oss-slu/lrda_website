f9d6a9449119e029455984ed508f856b16a33f1b
Izak Robles <izakrobles@gmail.com>
2023-09-16 09:03:07 -0500

    reconfigured new website with tailwind

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="">
+        <h1 className="text-blue-500 text-xl">Where's Religion?</h1>
+      </div>
+    </main>
+  );
+}
