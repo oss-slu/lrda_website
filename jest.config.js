c33b171f21a0e795f0c262a43854982849b29d02
yashb196 <113057400+yashb196@users.noreply.github.com>
2023-10-01 00:08:30 -0500

    fixing yml file

--- jest.config.js
+++ jest.config.js
@@ -2,19 +2,8 @@ const nextJest = require("next/jest");
 const createJestConfig = nextJest({
   dir: "./",
 });
-
 const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>/"],
   testEnvironment: "jest-environment-jsdom",
-  setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
-  transform: {
-    ...createJestConfig.transform,
-    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-  },
-  moduleNameMapper: {
-    ...createJestConfig.moduleNameMapper,
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
-  },
 };
-
 module.exports = createJestConfig(customJestConfig);
