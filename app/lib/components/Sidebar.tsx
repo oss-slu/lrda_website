bf9dae0be7d3fc43651109310de9422cd9255b7e
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-10-02 13:34:01 -0500

    Have an add note button

--- app/lib/components/Sidebar.tsx
+++ app/lib/components/Sidebar.tsx
@@ -4,17 +4,12 @@ import Link from 'next/link';
 
 const Sidebar = () => {
   return (
-    <div className="flex flex-col p-4 bg-gray-200 h-full">
-      {/* Other sidebar content can go here */}
-      
-      <Link href="/add-note">
-        <a className="mt-2 p-2 text-white bg-blue-500 hover:bg-blue-600 rounded">
-          Add Note
-        </a>
-      </Link>
-
-      {/* More sidebar content can go here */}
-    </div>
+    <div className="mt-2 p-2 text-white bg-blue-500 hover:bg-blue-600 rounded">
+    <Link href="/add-note">
+      Add Note
+    </Link>
+  </div>
+  
   );
 };
 
