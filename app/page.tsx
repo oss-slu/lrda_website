8350c27b299417073fbdf15132f16584380a5a7f
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-10-08 17:40:01 -0500

    Fixed it and merged from joshs branch

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,15 @@
 import Image from "next/image";
 import SearchBar from './components/searchBar';
 import Sidebar from "./lib/components/Sidebar";
-import RootLayout from "./layout";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="">
-        {/* Sidebar */}
+        <Sidebar />
         <h1 className="text-blue-500 text-xl mb-4">Where's Religion?</h1>
         <SearchBar />
       </div>
-    </RootLayout>
+    </main>
   );
 }
