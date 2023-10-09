9139836f3f02e37646e7689cfae98e88ce5ceafc
Izak Robles <izakrobles@gmail.com>
2023-10-09 09:16:59 -0500

    just trying to resolve conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="">
         <h1 className="text-blue-500 text-xl">Where's Religion?</h1>
-        <AddNotePage />
+        <AddNotePage /> {/* This is the note taking componenet */}
       </div>
     </main>
   );
