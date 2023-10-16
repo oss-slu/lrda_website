6555bef20d845ec73219e0542daca8411c73ab04
Izak Robles <izakrobles@gmail.com>
2023-10-16 11:08:54 -0500

    the test still does not pass. I need to do more research

--- app/__tests__/noteComponent.test.tsx
+++ app/__tests__/noteComponent.test.tsx
@@ -2,6 +2,15 @@ import React from "react";
 import NoteComponent from "../lib/components/noteComponent";
 import { render, screen, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import { RichUtils } from 'draft-js';
+
+jest.mock('draft-js', () => ({
+  ...jest.requireActual('draft-js'),
+  RichUtils: {
+    ...jest.requireActual('draft-js').RichUtils,
+    toggleInlineStyle: jest.fn(jest.requireActual('draft-js').RichUtils.toggleInlineStyle),
+  },
+}));
 
 describe("NoteComponent Component", () => {
   
@@ -15,12 +24,14 @@ describe("NoteComponent Component", () => {
       render(<NoteComponent />);
       const boldButton = screen.getByText("Bold");
       fireEvent.click(boldButton);
+      expect(RichUtils.toggleInlineStyle).toHaveBeenCalledWith(expect.anything(), 'BOLD');
     });
 
     it("toggles italic style when Italic button is clicked", () => {
       render(<NoteComponent />);
       const italicButton = screen.getByText("Italic");
       fireEvent.click(italicButton);
+      expect(RichUtils.toggleInlineStyle).toHaveBeenCalledWith(expect.anything(), 'ITALIC');
     });
   });
 });
