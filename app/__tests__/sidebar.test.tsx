484e5cc9dad4ac4f0b4910705e9f12d0fb9f4991
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-10-20 15:42:49 -0500

    Sidebar test cases work but the layout page is now failing

--- app/__tests__/sidebar.test.tsx
+++ app/__tests__/sidebar.test.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { useRouter } from 'next/router';
-import Sidebar from '../lib/components/Sidebar'; 
+import '@testing-library/jest-dom/extend-expect';
+import Sidebar from '../lib/components/Sidebar'; // Update the path to your Sidebar component accordingly
 
 jest.mock('next/router', () => ({
-  useRouter: jest.fn()
+  useRouter: jest.fn(),
 }));
 
 describe('Sidebar Component', () => {
-  let mockPush;
+  let mockPush: jest.Mock;
 
   beforeEach(() => {
     mockPush = jest.fn();
-    useRouter.mockImplementation(() => ({
+    (useRouter as jest.Mock).mockImplementation(() => ({
       push: mockPush,
     }));
   });
 
   it('renders the sidebar correctly', () => {
     render(<Sidebar />);
-    const sidebarElement = screen.getByRole('complementary'); // assuming the sidebar has role="complementary"
-    expect(sidebarElement).toBeInTheDocument();
+    // Query for a child element of the sidebar to confirm it's rendered
+    const linkElement = screen.getByText('Add Note');
+    expect(linkElement).toBeInTheDocument();
   });
 
   it('displays the "Add Note" link', () => {
--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
       <head />
+      <div>
       <body>{children}</body>
+      </div>
     </html>
   );
 }
