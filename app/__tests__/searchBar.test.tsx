908ce9b4fb53eb4b6efb066420cc607c5f1b60da
Josh Hogan <josh.hogan@slu.edu>
2023-10-22 21:14:40 -0500

    test cases for search bar component

--- app/__tests__/searchBar.test.tsx
+++ app/__tests__/searchBar.test.tsx
@@ -1,36 +1,75 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import SearchBar from '../components/SearchBar'; 
+import SearchBar from '../components/searchBar'; // Make sure to adjust the import path
+import "@testing-library/jest-dom";
+import { render, fireEvent } from '@testing-library/react';
 
 
-describe('SearchBar Component', () => {
-  it('renders without crashing', () => {
-    render(<SearchBar />);
-    const searchBarElement = screen.getByText('Search');
-    expect(searchBarElement).toBeInTheDocument();
-  });
-
-  it('initial input value is empty', () => {
-    render(<SearchBar />);
-    const inputElement = screen.getByPlaceholderText('Search...');
-    expect(inputElement).toHaveValue('');
-  });
-
-  it('updates input value when typed into', () => {
-    render(<SearchBar />);
-    const inputElement = screen.getByPlaceholderText('Search...');
-    fireEvent.change(inputElement, { target: { value: 'test input' } });
-    expect(inputElement).toHaveValue('test input');
-  });
-
-  it('calls handleSearch when the search button is clicked', () => {
-    const mockHandleSearch = jest.fn();
-    jest.spyOn(SearchBar.prototype, 'handleSearch').mockImplementation(mockHandleSearch);
-
-    render(<SearchBar />);
-    const searchButton = screen.getByText('Search');
-    fireEvent.click(searchButton);
-
-    expect(mockHandleSearch).toHaveBeenCalled();
-  });
+test('renders the SearchBar component', () => {
+  render(<SearchBar />);
 });
+;
+
+test('updates the search input when the user types', () => {
+  const { getByPlaceholderText } = render(<SearchBar />);
+  const searchInput = getByPlaceholderText('Search...'); // Assuming 'Search...' is the placeholder text
+
+  // Simulate user input
+  fireEvent.change(searchInput, { target: { value: 'Sample Query' } });
+
+  // Check if the search input value updates correctly
+  expect(searchInput).toHaveValue('Sample Query');
+});
+
+test('triggers search on pressing Enter key', () => {
+  const { getByPlaceholderText, getByTestId, queryByText } = render(<SearchBar />);
+  const searchInput = getByPlaceholderText('Search...'); // Assuming 'Search...' is the placeholder text
+  const searchButton = getByTestId('1234'); // Use your custom Test ID here
+
+  // Enter a search query
+  fireEvent.change(searchInput, { target: { value: 'Sample Query' } });
+
+  // Press the Enter key
+  fireEvent.keyPress(searchInput, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+});
+
+
+test('handles large amount of search input', () => {
+  const { getByPlaceholderText, getByTestId, queryByText } = render(<SearchBar />);
+  const searchInput = getByPlaceholderText('Search...'); // Assuming 'Search...' is the placeholder text
+  const searchButton = getByTestId('1234'); // Use your custom Test ID here
+
+  // Enter a large amount of text in the search input
+  const largeText = 'Lorem ipsum '.repeat(1000); // Creating a large text
+
+  fireEvent.change(searchInput, { target: { value: largeText } });
+
+  // Press the Enter key
+  fireEvent.keyPress(searchInput, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+  // Check if the search is aborted or if the component handles it gracefully
+  // You can check if there are any performance issues
+  expect(queryByText('Sample Query')).not.toBeInTheDocument();
+});
+
+test('handles search input with special characters', () => {
+  const { getByPlaceholderText, getByTestId, queryByText } = render(<SearchBar />);
+  const searchInput = getByPlaceholderText('Search...'); // Assuming 'Search...' is the placeholder text
+  const searchButton = getByTestId('1234'); // Use your custom Test ID here
+
+  // Enter a search query with special characters
+  const specialCharacters = '!@#$%^&*()';
+
+  fireEvent.change(searchInput, { target: { value: specialCharacters } });
+
+  // Press the Enter key
+  fireEvent.keyPress(searchInput, { key: 'Enter', code: 'Enter', charCode: 13 });
+
+  // Check if the search is handled correctly or if any special characters are sanitized
+  expect(queryByText('Sample Query')).not.toBeInTheDocument();
+});
+
+
+
+
+
+
--- app/components/searchBar.tsx
+++ app/components/searchBar.tsx
@@ -18,8 +18,15 @@ const SearchBar = () => {
       console.error('API Error:', error);
     }
   };
-  
 
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      // If Enter key is pressed, trigger the search
+      handleSearch();
+    }
+  };
+
+  
   return (
     <div className="flex items-center space-x-4">
       <input
@@ -32,6 +39,7 @@ const SearchBar = () => {
       <button
         className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
         onClick={handleSearch}
+        data-testid="1234" // Add the custom Test ID
       >
         Search
       </button>
