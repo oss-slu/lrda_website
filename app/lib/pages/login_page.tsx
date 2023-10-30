73e07dd91738031613837556ecb827df2f4bad06
Izak Robles <izakrobles@gmail.com>
2023-10-29 23:06:24 -0500

    v1 login screen(routing to said page nonfunctional)

---
+++ app/lib/pages/login_page.tsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [snackState, toggleSnack] = useState(false);
+
+    const onDismissSnackBar = () => toggleSnack(false);
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-[#F4DFCD]">
+            <div className="relative w-full h-full">
+                <Image src={"/splash.png"} alt="Background Image" layout="fill" objectFit="cover" />
+                <div className="absolute inset-0 flex flex-col items-center justify-center">
+                    <div className="w-3/4 bg-white p-8 rounded-lg shadow-lg">
+                        <h1 className="text-4xl font-bold mb-10 text-center">Login</h1>
+                        <div className="mb-4">
+                            <input
+                                type="text"
+                                placeholder="Username..."
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                                className="w-full p-3 border border-gray-300 rounded-lg"
+                            />
+                        </div>
+                        <div className="mb-4">
+                            <input
+                                type="password"
+                                placeholder="Password..."
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                className="w-full p-3 border border-gray-300 rounded-lg"
+                            />
+                        </div>
+                        <button className="mb-2 text-sm text-blue-500 hover:underline" >Forgot Password?</button>
+                        <h3>
+                            Login Button Goes Here
+                        </h3>
+                        <h3>
+                            Register Button Goes Here
+                        </h3>
+                    </div>
+                    {snackState && (
+                        <div className="fixed bottom-8 bg-white text-center p-3 rounded-lg">
+                            Invalid User Credentials
+                            <button className="ml-4 text-blue-500" onClick={onDismissSnackBar}>
+                                Dismiss
+                            </button>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
-import Image from "next/image";
-import SearchBar from './lib/components/search_bar';
-import AddNotePage from "./lib/pages/add_note_page";
+import SearchBar from "./lib/components/search_bar";
 import Sidebar from "./lib/components/side_bar";
 import NoteComponent from "./lib/components/note_component";
-
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <div>
+        <Link className="text-blue-500 text-xl mb-4" href="/" >Login</Link>
+      </div>
       <div className="">
         <Sidebar />
         <h1 className="text-blue-500 text-xl mb-4">Where's Religion?</h1>
