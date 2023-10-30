8ad7c7ab811140edeaac33f448184e534e0a34e0
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-10-30 13:31:27 -0500

    Added the login button component in the components folder

---
+++ app/lib/components/login_button.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { User } from '../../models/user_class'; // Assuming the User class is at the same path as in React Native
+import styles from './LoginButton.module.css'; // Assuming you have a CSS module for styling
+
+type LoginButtonProps = {
+  username: string;
+  password: string;
+  onLoginStatus: (status: string) => void;
+  onError: () => void;
+};
+
+const user = User.getInstance();
+
+const LoginButton: React.FC<LoginButtonProps> = ({ username, password, onLoginStatus, onError }) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleLogin = async () => {
+    if (username === '' || password === '') {
+      onError(); // Function to handle empty fields, likely showing an error message
+      return;
+    }
+
+    setIsLoading(true);
+    try {
+      const status = await user.login(username, password);
+      onLoginStatus(status); // Callback to update the parent component about login status
+      setIsLoading(false);
+    } catch (error) {
+      onError(); // Function to handle errors, likely showing an error message
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <button onClick={handleLogin} className={styles.loginButton} disabled={isLoading}>
+      {isLoading ? 'Loading...' : 'Login'}
+    </button>
+  );
+};
+
+export default LoginButton;
