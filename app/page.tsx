2d66b23f4854f536c6b496c890fee4c62bd7f836
Izak Robles <izakrobles@gmail.com>
2023-11-06 15:33:32 -0600

    fixed the user class caching

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <div>
-        <Link className="text-blue-500 text-xl mb-4" href="/lib/pages/loginPage" >Login</Link>
-      </div>
       <div className="">
         <Sidebar />
         <h1 className="text-blue-500 text-xl mb-4">Where's Religion?</h1>
