09d9772894af5d7f10662ca0c5d39a194a5baddf
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-11-05 20:59:59 -0600

    Just reset the Sidebar page since we don't need so much functionality on the SideBar

--- app/lib/components/side_bar.tsx
+++ app/lib/components/side_bar.tsx
@@ -1,54 +1,14 @@
-import { useState, useEffect } from "react";
-import Link from "next/link";
-import { User } from "../models/user_class";
+// components/Sidebar.tsx
 
-const user = User.getInstance();
+import Link from 'next/link';
 
 const Sidebar = () => {
-  const [name, setName] = useState<string | null>(null);
-
-  const handleLogout = () => {
-    localStorage.removeItem(name || "");
-    user.logout();
-    window.location.href = "/";
-  };
-
-  useEffect(() => {
-    const fetchName = async () => {
-      const userName = await user.getName();
-      setName(userName);
-      const item = userName ? localStorage.getItem(userName) : null;
-      user.login(userName || "", item || "");
-    };
-
-    fetchName();
-  }, []);
   return (
     <div className="fixed top-0 left-0 bottom-0 w-64 bg-gray-200 p-4 overflow-y-auto">
-      {name ? (
-        <div className="flex justify-between items-center mb-4">
-          <span className="text-lg font-semibold leading-normal">
-            Hi, {name}!
-          </span>
-          <button
-            className="text-blue-500 text-m leading-normal p-0"
-            onClick={handleLogout}
-          >
-            Logout
-          </button>
-        </div>
-      ) : (
-        <div>
-          <Link
-            className="text-blue-500 text-xl mb-4"
-            href="/lib/pages/loginPage"
-          >
-            Login
-          </Link>
-        </div>
-      )}
       <div className="mt-2 p-2 text-white bg-blue-500 hover:bg-blue-600 rounded">
-        <Link href="/add-note">Add Note</Link>
+        <Link href="/add-note">
+          Add Note
+        </Link>
       </div>
       {/* Add more buttons or content here */}
     </div>
