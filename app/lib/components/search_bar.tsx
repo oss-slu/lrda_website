71c5f393626437232f4dc961f2346361b706be1c
Izak Robles <izakrobles@gmail.com>
2023-11-13 10:31:46 -0600

    correction

--- app/lib/components/search_bar.tsx
+++ app/lib/components/search_bar.tsx
@@ -30,7 +30,7 @@ const SearchBar = () => {
         onChange={(e) => setSearchText(e.target.value)}
         onKeyPress={handleKeyPress} 
       />
-      <button
+      <Button
         className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
         onClick={handleSearch}
         data-testid="search-button"
