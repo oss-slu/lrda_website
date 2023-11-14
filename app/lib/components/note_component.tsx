5a08742b3fb8a8a2798d53e93c4f208c3bfca916
Izak Robles <izakrobles@gmail.com>
2023-11-14 16:21:19 -0600

    added icons

--- app/lib/components/note_component.tsx
+++ app/lib/components/note_component.tsx
@@ -2,7 +2,13 @@
 import { Editor, EditorState, RichUtils } from "draft-js";
 import "draft-js/dist/Draft.css";
 import { useState, useEffect } from "react";
-import { stateToHTML } from 'draft-js-export-html';
+import { stateToHTML } from "draft-js-export-html";
+import {
+  FontBoldIcon,
+  FontItalicIcon,
+  UnderlineIcon,
+} from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
 
 export default function NoteComponent() {
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
@@ -16,14 +22,14 @@ export default function NoteComponent() {
 
   useEffect(() => {
     setPlainText(editorState.getCurrentContent().getPlainText());
-  }, [editorState])
+  }, [editorState]);
 
   useEffect(() => {
     // Updates the rawHTML component as the editorState changes
     const html = stateToHTML(editorState.getCurrentContent());
     setRawHTML(html);
   }, [editorState]);
-  
+
   const handleKeyCommand = (command: any) => {
     const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
@@ -49,15 +55,24 @@ export default function NoteComponent() {
     <main className="flex flex-grow min-h-screen flex-col items-center justify-between p-6 lg:p-24">
       <div className="w-full max-w-4xl">
         <div className="flex border-b border-black p-2">
-          <button onClick={toggleBold} className="border border-black px-3 py-1 m-1 rounded text-black">
-            Bold
-          </button>
-          <button onClick={toggleItalic} className="border border-black px-3 py-1 m-1 rounded text-black">
-            Italic
-          </button>
-          <button onClick={toggleUnderline} className="border border-black px-3 py-1 m-1 rounded text-black">
-            Underline
-          </button>
+          <Button
+            onClick={toggleBold}
+            className="border w-10 h-10 bg-secondary border-black px-3 py-1 m-1 rounded text-black"
+          >
+            <FontBoldIcon />
+          </Button>
+          <Button
+            onClick={toggleItalic}
+            className="border w-10 h-10 bg-secondary border-black px-3 py-1 m-1 rounded text-black"
+          >
+            <FontItalicIcon />
+          </Button>
+          <Button
+            onClick={toggleUnderline}
+            className="border w-10 h-10 bg-secondary border-black px-3 py-1 m-1 rounded text-black"
+          >
+            <UnderlineIcon />
+          </Button>
         </div>
         {isClient && (
           <div className="mt-2 border border-black p-4 rounded-lg min-h-[300px] w-full bg-white">
--- app/lib/components/search_bar.tsx
+++ app/lib/components/search_bar.tsx
@@ -1,31 +1,33 @@
-'use client'
-import React, { useState } from 'react';
-import ApiService from '../utils/api_service';
-import { Button } from "@/components/ui/button"
+"use client";
+import React, { useState } from "react";
+import ApiService from "../utils/api_service";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 const SearchBar = () => {
   const [searchText, setSearchText] = useState("");
 
   const handleSearch = async () => {
     console.log("Search text:", searchText);
-    if (!searchText) {  
+    if (!searchText) {
       console.log("Search text is empty. Aborting search.");
       return;
     }
     try {
       const response = await ApiService.searchMessages(searchText);
-      console.log('API Response:', response);
+      console.log("API Response:", response);
     } catch (error) {
-      console.error('API Error:', error);
+      console.error("API Error:", error);
     }
   };
 
   return (
-    <div className="flex items-center space-x-4 min-w-300">
-      <input
+    <div className="flex items-center w-full min-w-max">
+      <Input
         type="text"
         placeholder="Search..."
-        className="border border-gray-300 rounded-md p-2 text-black"
+        className="border border-gray-300 rounded-md p-2 flex-grow"
         value={searchText}
         onChange={(e) => setSearchText(e.target.value)}
       />
@@ -33,16 +35,11 @@ const SearchBar = () => {
         className="px-4 py-2 rounded-md"
         onClick={handleSearch}
         data-testid="search-button"
-        
       >
-        Search
+          <MagnifyingGlassIcon className="w-6 h-6" />
       </Button>
     </div>
   );
 };
 
 export default SearchBar;
-
-
-
-
