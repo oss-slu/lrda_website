1d5575fcfd52192373018d842552e51600b64114
Izak Robles <izakrobles@gmail.com>
2023-12-26 09:21:23 -0600

    resolve local storage error

--- app/lib/components/navbar.tsx
+++ app/lib/components/navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar() {
           </div>
         ) : (
           <Button
-            onClick={() => (window.location.href = "lib/pages/loginPage")}
+            onClick={() => (window.location.href = "/lib/pages/loginPage")}
             className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 border border-blue-700 rounded shadow"
           >
             Login
--- app/lib/utils/async_storage.ts
+++ app/lib/utils/async_storage.ts
@@ -1,27 +1,30 @@
-
-
 export const setItem = (key: string, value: string) => {
-    try {
-      localStorage.setItem(key, value);
-    } catch (error) {
-      console.log('Error storing value: ', error);
-    }
-  };
-  
-  export const getItem = (key: string) => {
-    try {
-      const value = localStorage.getItem(key);
-      return value;
-    } catch (error) {
-      console.log('Error retrieving value: ', error);
-    }
-  };
-  
-  export const removeItem = (key: string) => {
-    try {
-      localStorage.removeItem(key);
-    } catch (error) {
-      console.log('Error deleting value: ', error);
-    }
-  };
-  
+  if (typeof window !== 'undefined') {
+      try {
+          localStorage.setItem(key, value);
+      } catch (error) {
+          console.log('Error storing value: ', error);
+      }
+  }
+};
+
+export const getItem = (key: string) => {
+  if (typeof window !== 'undefined') {
+      try {
+          return localStorage.getItem(key);
+      } catch (error) {
+          console.log('Error retrieving value: ', error);
+      }
+  }
+  return null;
+};
+
+export const removeItem = (key: string) => {
+  if (typeof window !== 'undefined') {
+      try {
+          localStorage.removeItem(key);
+      } catch (error) {
+          console.log('Error removing value: ', error);
+      }
+  }
+};
