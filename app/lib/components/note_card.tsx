fd41d196b0cdfdfc0c94c547fe659a559feea07e
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-12-27 00:11:40 -0600

    notes with media now render

--- app/lib/components/note_card.tsx
+++ app/lib/components/note_card.tsx
@@ -7,21 +7,27 @@ interface NoteCardProps {
 }
 
 const NoteCard: React.FC<NoteCardProps> = ({ note }) => {
-  // This is just a placeholder. Replace with actual properties from your Note type.
   const  title  = note.title;
+  const text = note.text;
+  const imageMedia = note.media.filter(media => media.type === 'image')[0];
 
   return (
-    <div className="note-card-container bg-white rounded-lg shadow-md overflow-hidden">
-      {/* <img src={imageUrl} alt={title} className="w-full h-32 object-cover" /> */}
-      <div className="p-4">
-        <div className="flex justify-between items-center">
-          <h3 className="text-lg font-semibold">{title}</h3>
-          <span className="text-sm">{"04/19/1972"}</span>
+    <div className="note-card-container bg-white rounded-lg shadow-lg overflow-hidden flex flex-col" style={{ width: '280px', height: '280px' }}>
+      <div className="flex-shrink-0">
+      {imageMedia && (
+        <div className="flex-shrink-0">
+          <img src={imageMedia.uri} alt={note.title} className="w-full h-48 object-cover" />
         </div>
-        <p className="text-sm">{"123 Grand Blvd"}</p>
-        <p className="text-lg font-bold">{"$5"}</p>
-        <p className="text-sm line-clamp-2">{"Lovely day huh"}</p>
-        {/* Add more details as required */}
+      )}
+      </div>
+      <div className="p-4 flex flex-col justify-between flex-grow">
+        <h3 className="text-lg font-semibold truncate">{title}</h3>
+        <p className="text-sm text-gray-500">{"1/22/3333"}</p>
+        <p className="text-sm text-gray-500 truncate">{"123 Grand Blvd"}</p>
+        <div className="flex justify-between items-end mt-2">
+          <p className="text-lg font-bold">{"$500,000"}</p>
+          <p className="text-sm line-clamp-3">{"I can't render HTML haha"}</p>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,11 @@
 import React, { useState, useEffect } from "react";
 import Sidebar from "./lib/components/side_bar";
 import NoteEditor from "./lib/components/note_component";
-import NoteCard from "./lib/components/note_card";
 import { Note } from "./types";
 
 export default function Home() {
   const [isClient, setIsClient] = useState(false);
   const [selectedNote, setSelectedNote] = useState<Note>();
-  const [NoteComponent, setNoteComponent] = useState<React.ElementType | null>(
-    null
-  );
 
   const handleNoteSelect = (note: Note) => {
     setSelectedNote(note);
@@ -18,27 +14,15 @@ export default function Home() {
 
   useEffect(() => {
     setIsClient(true);
-    // Dynamically import the Note component based on the window location
-    async function loadComponent() {
-      if (window.location.pathname.includes("/map/")) {
-        setNoteComponent(() => NoteCard);
-      } else {
-        setNoteComponent(() => NoteEditor);
-      }
-    }
-
-    if (process.browser) {
-      loadComponent();
-    }
   }, []);
+
 
   return (
     <main className="relative flex h-screen flex-row p-4">
       <Sidebar onNoteSelect={handleNoteSelect} />
       <div className="flex-1 ml-64">
-        {isClient && NoteComponent ? (
-          <NoteComponent note={selectedNote} />
-        ) : null}
+
+      {isClient && <NoteEditor note={selectedNote} />}
       </div>
     </main>
   );
