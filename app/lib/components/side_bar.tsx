eb0006b21c1abc43fc4dc017f5dc86f8d5fcf049
Izak Robles <izakrobles@gmail.com>
2023-12-29 15:25:58 -0600

    test

--- app/lib/components/side_bar.tsx
+++ app/lib/components/side_bar.tsx
@@ -81,7 +81,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onNoteSelect }) => {
   };
 
   return (
-    <div className="absolute top-0 left-0 h-[90vh] w-64 bg-gray-200 p-4 overflow-y-auto flex flex-col">
+    <div className="absolute top-0 left-0 h-[90vh] w-64 bg-gray-200 p-4 overflow-y-auto flex flex-col z-30">
       <div className="w-full">
         <SearchBar onSearch={handleSearch} />
       </div>
