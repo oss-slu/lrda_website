632e1f127d271bc95bf1f515d98f6da64405adb5
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2023-12-29 17:01:19 -0600

    Time to toast

--- app/lib/components/note_card.tsx
+++ app/lib/components/note_card.tsx
@@ -5,9 +5,24 @@ interface NoteCardProps {
   note: Note;
 }
 
+function formatDateTime(date: Date) {
+  if (!date) return 'Pick a date';
+
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
+  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+  const ampm = hours < 12 ? 'AM' : 'PM';
+
+  return `${date.toDateString()} ${formattedHours}:${formattedMinutes} ${ampm}`;
+}
+
 const NoteCard: React.FC<NoteCardProps> = ({ note }) => {
   const title = note.title;
+  const date = formatDateTime(note.time);
   const text = note.text;
+  const creator = note.creator;
+  const tags: string[] = note.tags;
   const imageMedia = note.media.filter(media => media.type === 'image')[0];
 
   return (
@@ -21,11 +36,16 @@ const NoteCard: React.FC<NoteCardProps> = ({ note }) => {
       )}
       <div className="p-4 flex flex-col justify-between flex-grow">
         <h3 className="text-lg font-semibold truncate">{title}</h3>
-        <p className="text-sm text-gray-500">{"1/22/3333"}</p>
-        <p className="text-sm text-gray-500 truncate">{"123 Grand Blvd"}</p>
+        <p className="text-sm text-gray-500">{date.toString()}</p> 
+         <div className="flex flex-wrap gap-2">
+          {tags.map((tag, index) => (
+            <span key={index} className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800">
+              {tag}
+            </span>
+          ))}
+        </div>
         <div className="flex justify-between items-end mt-2">
-          <p className="text-lg font-bold">{"$500,000"}</p>
-          <p className="text-sm line-clamp-3">{"I can't render HTML"}</p>
+          <p className="text-lg font-bold">{note.creator}</p>
         </div>
       </div>
     </div>
