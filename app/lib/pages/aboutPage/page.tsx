37048f40c0adbc6292946fdcd443cc85dbeb621d
Josh Hogan <josh.hogan@slu.edu>
2024-02-10 19:56:38 -0600

    able to navigate to the about page using the about link on the nav bar

---
+++ app/lib/pages/aboutPage/page.tsx
@@ -0,0 +1,18 @@
+// pages/aboutPage/page.tsx
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      textAlign: 'center'
+    }}>
+      <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Hello</h1>
+    </div>
+  );
+};
+
+export default Page;
