ffbb08c93d8ac570a7cf882588865182c8705300
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2024-02-26 13:10:24 -0600

    Updated postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  plugins: {
-    'postcss-import': {},
-    'postcss-nesting': {},
-    'tailwindcss': {},
-    'autoprefixer': {},
-  },
-};
+    plugins: {
+      'postcss-import': {},
+      'postcss-nesting': {},
+      'tailwindcss': {},
+      'autoprefixer': {},
+    },
+  };
