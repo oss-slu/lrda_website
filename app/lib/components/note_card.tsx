45d43167d48189e596bf403b901c99336825e351
Izak Robles <izakrobles@gmail.com>
2024-03-30 13:59:17 -0500

    Resolved Merge Conflict NoteCard

--- app/lib/components/note_card.tsx
+++ app/lib/components/note_card.tsx
@@ -12,18 +12,6 @@ interface NoteCardProps {
   note: Note;
 }
 
-function formatDateTime(date: Date) {
-  if (!date) return 'Pick a date';
-
-  const hours = date.getHours();
-  const minutes = date.getMinutes();
-  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-  const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-  const ampm = hours < 12 ? 'AM' : 'PM';
-
-  return `${date.toDateString()} ${formattedHours}:${formattedMinutes} ${ampm}`;
-}
-
 const NoteCard: React.FC<NoteCardProps> = ({ note }) => {
   const title = note.title;
   const date = formatDateTime(note.time);
