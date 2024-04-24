148e5099059677083303e7c2a97e373dd76c133d
Josh Hogan <josh.hogan@slu.edu>
2024-04-24 10:23:47 -0500

    re-commented out about page link on the navbar. still waiting on Dr. Park to send content for the page

--- app/lib/components/navbar.tsx
+++ app/lib/components/navbar.tsx
@@ -272,11 +272,13 @@ export default function Navbar() {
           </Link>
         ) : null}
 
-        { <Link legacyBehavior href="/lib/pages/aboutPage" passHref>
-          <a className="text-2xl font-bold text-blue-300 hover:text-blue-500 transition duration-300 ease-in-out mr-2">
-            About
-          </a>
-        </Link> }
+       {/*
+        <Link legacyBehavior href="/lib/pages/aboutPage" passHref>
+        <a className="text-2xl font-bold text-blue-300 hover:text-blue-500 transition duration-300 ease-in-out mr-2">
+        About
+        </a>
+        </Link>
+        */}
 
         <Button
         onClick={startTour}
