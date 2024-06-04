9b80d2b7c718de2a8a53f769adf58a0057a27a05
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2024-06-04 15:18:19 -0500

    Signup screen basics are started and firebase installed

---
+++ app/lib/pages/signupPage/page.tsx
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import RegisterButton from '../../components/register_button';
+import { toast } from 'sonner';
+
+const SignupPage: React.FC = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  // Add your Firebase sign-up logic here
+
+  return (
+    <div className="flex flex-col items-center justify-center bg-[#F4DFCD]">
+      <div className="flex items-center justify-center">
+        <Image
+          src="/splash.png"
+          alt="Background Image"
+          width="2080"
+          height="300"
+        />
+      </div>
+      <div className="absolute inset-10 flex flex-col items-center justify-center">
+        <div className="w-3/4 bg-white p-8 rounded-lg shadow-lg">
+          <h1 className="text-black-500 font-bold mb-20 text-center text-3xl">
+            Sign Up
+          </h1>
+          <div className="mb-4">
+            <input
+              type="text"
+              placeholder="Username..."
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="w-full p-3 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              placeholder="Password..."
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-3 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              type="password"
+              placeholder="Confirm Password..."
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full p-3 border border-gray-300 rounded-lg"
+            />
+          </div>
+          <div className="flex flex-col sm:flex-row items-center justify-center">
+            <RegisterButton />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignupPage;
