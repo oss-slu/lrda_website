5c87b39646e024c2e2b49ca4fac0e4f2541bd114
Stuartwastaken <88628337+Stuartwastaken@users.noreply.github.com>
2024-07-23 16:10:45 -0500

    Generate tags CSS is much better now

--- app/lib/components/noteElements/note_component.tsx
+++ app/lib/components/noteElements/note_component.tsx
@@ -328,6 +328,15 @@ export default function NoteEditor({
               <Music className="text-current" />
               <div className="ml-2">Audio</div>
             </button>
+            <div className="w-2 h-9 bg-border" />
+            <button
+              className="hover:text-purple-500 flex justify-center items-center w-full"
+              onClick={fetchSuggestedTags}
+            >
+              <div className="px-4 py-2 bg-white-200 text-black rounded-md hover:bg-purple-300 transition-all shadow-sm font-medium">
+                Generate Tags
+              </div>
+            </button>
           </div>
           <TagManager
             inputTags={noteState.tags}
@@ -336,15 +345,9 @@ export default function NoteEditor({
               handleTagsChange(noteHandlers.setTags, newTags)
             }
           />
-          <button
-            className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition-colors"
-            onClick={fetchSuggestedTags}
-          >
-            Generate Tags
-          </button>
           {loadingTags && <p>Loading suggested tags...</p>}
         </div>
-
+  
         {isAudioModalOpen && (
           <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="bg-white p-4 rounded-lg shadow-lg max-w-md mx-auto">
@@ -393,4 +396,5 @@ export default function NoteEditor({
       </div>
     </ScrollArea>
   );
+  
 }
