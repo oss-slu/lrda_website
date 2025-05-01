dc4c79d8f0f729359f9b5afbe5825fadf2ed0ded
Zanxiang Wang <zanxiang.wang@outlook.com>
2025-04-30 22:30:59 -0500

    comments

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
   maxWorkers: "50%",
   moduleNameMapper: {
-    "^@/app/(.*)$": "<rootDir>/app/$1",           // ✅ fix this line
+    "^@/app/(.*)$": "<rootDir>/app/$1",           // fix this line
     "^@/components/(.*)$": "<rootDir>/app/lib/components/$1",  // Optional if needed
     "^@/utils/(.*)$": "<rootDir>/app/utils/$1",   // Optional if needed
   },
