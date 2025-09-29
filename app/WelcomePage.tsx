b855d204ed365454b6c9c49779e23c59b7b5126c
Puneet Sontha <punster5000@gmail.com>
2025-09-29 15:51:20 -0500

    Changed layout of home page, and blended navigation bar.

--- app/WelcomePage.tsx
+++ app/WelcomePage.tsx
@@ -4,28 +4,34 @@ import Image from "next/image";
 
 const WelcomePage: React.FC = () => {
   return (
-    <div className="relative flex justify-center items-center h-full overflow-hidden">
-      {/* Centered Background Image */}
-      <div className="absolute inset-0 flex justify-center items-center">
-        <Image
-          src="/splash.png"
-          alt="Background Image"
-          layout="fill"
-          objectFit="cover"
-          className="z-[-1]"
-        />
+    <div className="flex flex-col md:flex-row flex h-screen">
+      {/* Left Side - Text */}
+      <div className="flex-1 flex flex-col justify-start bg-gray-900">
+        {/* Nested content with padding */}
+        <div className="pt-20 px-8 md:px-16">
+          <h1 className="text-5xl md:text-7xl font-extrabold text-white mb-6" style={{ textShadow: "2px 2px 4px rgba(0,0,0,0.3)" }}>
+          Where's Religion?
+          </h1>
+          {/* Sub-description */}
+          <p className="text-md md:text-xl text-gray-500 max-w-xl">
+            A revolutionary platform to support in-person research, remote data entry, media sharing, and mapping.
+          </p>
+        </div>
       </div>
 
-      {/* Centered "Where's Religion?" Text */}
-      <div className="text-center">
-        <h1 className="text-black text-9xl font-extrabold" style={{ 
-          textShadow: "2px 2px 4px rgba(255, 255, 255, 0.8)" 
-        }}>
-          Where's Religion?
-        </h1>
+      {/* Right Side - Image */}
+      <div className="flex-1 relative h-80 md:h-auto">
+        <Image
+          src="/splash.png"
+          alt="Right Side Image"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
     </div>
   );
 };
 
 export default WelcomePage;
+
+
