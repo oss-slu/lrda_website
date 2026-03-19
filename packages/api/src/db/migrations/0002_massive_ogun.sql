a6d532cb0fd24df32bb27ef496e11f1d0e434d34
Jacob Maynard <jacobamaynard@proton.me>
2026-03-19 15:53:49 -0500

    remove old migration files, add excalidraw

--- packages/api/src/db/migrations/0002_massive_ogun.sql
+++
@@ -1,9 +0,0 @@
-CREATE INDEX "account_user_id_idx" ON "account" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "audio_note_id_idx" ON "audio" USING btree ("note_id");--> statement-breakpoint
-CREATE INDEX "comment_note_id_idx" ON "comment" USING btree ("note_id");--> statement-breakpoint
-CREATE INDEX "comment_author_id_idx" ON "comment" USING btree ("author_id");--> statement-breakpoint
-CREATE INDEX "comment_thread_id_idx" ON "comment" USING btree ("thread_id");--> statement-breakpoint
-CREATE INDEX "media_note_id_idx" ON "media" USING btree ("note_id");--> statement-breakpoint
-CREATE INDEX "note_creator_id_idx" ON "note" USING btree ("creator_id");--> statement-breakpoint
-CREATE INDEX "session_user_id_idx" ON "session" USING btree ("user_id");--> statement-breakpoint
-CREATE INDEX "user_instructor_id_idx" ON "user" USING btree ("instructor_id");
